d9816507fa5daab50fd2bd7d5bddc8d4ba6bf4e4
omarhark <141445140+omarhark@users.noreply.github.com>
2023-11-23 19:42:11 +0100

    Update and adding some features in code base

--- src/Steps.jsx
+++ src/Steps.jsx
@@ -29,25 +29,37 @@ export default function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
+              <span>👈🏾</span>Previous
+            </Button>
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+              Next<span>👉🏾</span>
+            </Button>
           </div>
         </div>
       )}
     </>
+  );
+}
+function StepMessage({ children, step }) {
+  return (
+    <p className="message">
+      Step {step}: {children}
+    </p>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
